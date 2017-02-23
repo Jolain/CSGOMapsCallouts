8c89b94b8bbc9da15eb88f2568ecbbe37f39752c
Jolain Poirier <Jolain@users.noreply.github.com>
2017-02-23 18:53:21 -0400

    Added activities and associations to them

--- app/src/main/java/com/dorner/csgomapscallouts/activities/MainActivity.java
+++ app/src/main/java/com/dorner/csgomapscallouts/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dorner.csgomapscallouts.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.dorner.csgomapscallouts.R;
@@ -13,12 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button_mirage = (Button) findViewById(R.id.de_mirage);
-        Button button_dust2 = (Button) findViewById(R.id.de_dust2);
-        Button button_italy = (Button) findViewById(R.id.cs_italy);
-        Button button_cache = (Button) findViewById(R.id.de_cache);
-        Button button_cobble = (Button) findViewById(R.id.de_cobblestone);
         Button button_dust = (Button) findViewById(R.id.de_dust);
         Button button_inferno = (Button) findViewById(R.id.de_inferno);
         Button button_nuke = (Button) findViewById(R.id.de_nuke);
@@ -26,4 +22,57 @@ public class MainActivity extends AppCompatActivity {
         Button button_season = (Button) findViewById(R.id.de_season);
         Button button_train = (Button) findViewById(R.id.de_train);
     }
+
+    public void onButtonClick(View view) {
+        int id = view.getId();
+
+        switch (id) {
+            case R.id.de_mirage: {
+                Intent intent = new Intent(getApplicationContext(), MirageActivity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+            case R.id.de_dust2: {
+                Intent intent = new Intent(getApplicationContext(), Dust2Activity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+            case R.id.cs_italy: {
+                Intent intent = new Intent(getApplicationContext(), ItalyActivity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+            case R.id.de_cache: {
+                Intent intent = new Intent(getApplicationContext(), CacheActivity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+            case R.id.de_cobblestone: {
+                Intent intent = new Intent(getApplicationContext(), CobbleActivity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+            case R.id.de_dust: {
+                Intent intent = new Intent(getApplicationContext(), DustActivity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+            case R.id.de_inferno: {
+                Intent intent = new Intent(getApplicationContext(), InfernoActivity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+            case R.id.de_nuke: {
+                Intent intent = new Intent(getApplicationContext(), NukeActivity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+            case R.id.de_overpass: {
+                Intent intent = new Intent(getApplicationContext(), OverpassActivity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+            case R.id.de_season: {
+                Intent intent = new Intent(getApplicationContext(), SeasonActivity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+            case R.id.de_train: {
+                Intent intent = new Intent(getApplicationContext(), TrainActivity.class); //Application Context and Activity
+                startActivityForResult(intent, 0);
+            }
+        }
+    }
+
+
 }
